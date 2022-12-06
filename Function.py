ac62473c4cf03264e7cb10e8d44f8a63ad061841
Chunws <32253467+Chunws13@users.noreply.github.com>
2022-12-06 14:08:42 +0900

    Create Function.py

---
+++ Function.py
@@ -0,0 +1,32 @@
+# 2차원 배열 회전
+def rotate(o_list, index):
+    n = len(o_list) # 행 길이 계산
+    m = len(o_list[0]) # 열 길이 계산
+    
+    # 270도 회전
+    if index == 1:
+        new = [[0] * n for _ in range(m)]
+        for i in range(n):
+            for j in range(m):
+                new[m-j-1][i] = o_list[i][j]
+        return new
+    
+    # 180도 회전
+    elif index == 2:
+        new = [[0] * n for _ in range(m)]
+        for i in range(n):
+            for j in range(m):
+                new[m-j-1][n-i-1] = o_list[i][j]
+        return new
+    
+    
+    # 90도 회전
+    elif index == 3: 
+        new = [[0] * n for _ in range(m)]
+        for i in range(n):
+            for j in range(m):
+                new[j][n-i-1] = o_list[i][j]
+        return new
+    
+    else:
+        return o_list    
